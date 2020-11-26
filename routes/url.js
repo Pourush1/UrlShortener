f137d3a1041c82471624756271e3094e96e3cb27
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-11-25 21:30:20 -0800

    used short url library to get the short url code and used that to create a short Url, which was then saved to database. Also, redirected the short url to the original url.

---
+++ routes/url.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const validUrl = require('valid-url');
+const shortid = require('shortid');
+const config = require('config');
+
+const Url = require('../models/Url');
+
+router.post('/shorten', async (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get('baseUrl');
+
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json('Invalid base url');
+  }
+
+  // Create url code
+  const shortUrlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + '/' + shortUrlCode;
+
+        url = new Url({
+          longUrl,
+          shortUrl,
+          shortUrlCode,
+          date: new Date()
+        });
+
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Server error');
+    }
+  } else {
+    res.status(401).json('Invalid long url');
+  }
+});
+
+module.exports = router;
